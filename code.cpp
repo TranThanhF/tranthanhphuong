bbbcb1ec953d6eb2184e6edb9c58f5b469b4d380
dtung9903 <duytung9903@gmail.com>
2024-11-16 00:16:00 +0700

    cmtkhi

--- code.cpp
+++ code.cpp
@@ -7,27 +7,7 @@ namespace TinhTong
         static void Main(string[] args)
         {
             // Nhập số thứ nhất
-            Console.Write("Nhập số thứ nhất: ");
-            double so1 = Convert.ToDouble(Console.ReadLine());
-
-            // Nhập số thứ hai
-            Console.Write("Nhập số thứ hai: ");
-            double so2 = Convert.ToDouble(Console.ReadLine());
-
-            // Tính tổng
-            double tong = so1 + so2;
-
-            // Hiển thị kết quả
-            Console.WriteLine($ "Tổng của {so1} và {so2} là: {tong}");
-
-            double tich = so1 * so2;
-
-            // Hiển thị kết quả
-            Console.WriteLine($ "tich của {so1} và {so2} là: {tich}");
-
-            // Đợi người dùng nhấn phím trước khi thoát
-            Console.WriteLine("Nhấn phím bất kỳ để thoát...");
-            Console.ReadKey();
+            return abc;
         }
     }
 }
