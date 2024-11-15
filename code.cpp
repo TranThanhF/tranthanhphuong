9626bf057706cc468edaffe2541eaa3a327e3812
dtung9903 <duytung9903@gmail.com>
2024-11-16 01:03:40 +0700

    cc

--- code.cpp
+++ code.cpp
@@ -7,15 +7,8 @@ int main()
 {
     int num1, num2;
     cout << "khi 3";
-
+    cout << " mic check mic check mic check";
     // Nhập hai số nguyên từ người dùng
-    cout << "Nhập số thứ nhất: ";
-    cin >> num1;
-    cout << "Nhập số thứ hai: ";
-    cin >> num2;
-
-    // Tính và in ra UCLN
-    cout << "Ước chung lớn nhất của " << num1 << " và " << num2 << " là: " << UCLN(num1, num2) << endl;
 
     return 0;
 }
